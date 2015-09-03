19994e6a43eb95e3ffc074ed20345ed3f348d3b3
Christopher Hicks <chicks.net@gmail.com>
2015-09-03 04:25:44 -0700

    python: please pylint :zzz:

--- bin/chart_steam.py
+++ bin/chart_steam.py
@@ -17,46 +17,46 @@ import matplotlib
 #
 # sqlalchemy classes
 #
- 
+
 Base = declarative_base()
- 
+
 class Game(Base):
-    __tablename__ = 'game'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
-    name_short = Column(String(250))
-    app_id = Column(Integer)
-    achievements = Column(Boolean)
-    stats = Column(Boolean)
-    leaderboards = Column(Boolean)
-    global_achievements = Column(Boolean)
-    global_leaderboards = Column(Boolean)
-    url_achievements = Column(String(250))
-    url_logo = Column(String(250))
-    url_store = Column(String(250))
- 
+	__tablename__ = 'game'
+	# Here we define columns for the table person
+	# Notice that each column is also a normal Python instance attribute.
+	id = Column(Integer, primary_key=True)
+	name = Column(String(250), nullable=False)
+	name_short = Column(String(250))
+	app_id = Column(Integer)
+	achievements = Column(Boolean)
+	stats = Column(Boolean)
+	leaderboards = Column(Boolean)
+	global_achievements = Column(Boolean)
+	global_leaderboards = Column(Boolean)
+	url_achievements = Column(String(250))
+	url_logo = Column(String(250))
+	url_store = Column(String(250))
+
 class GameStats(Base):
-    __tablename__ = 'game_stats'
-    id = Column(Integer, primary_key=True)
-    game_id = Column(Integer, ForeignKey('game.id'))
-    game = relationship(Game)
+	__tablename__ = 'game_stats'
+	id = Column(Integer, primary_key=True)
+	game_id = Column(Integer, ForeignKey('game.id'))
+	game = relationship(Game)
 
 class SteamProfile(Base):
-    __tablename__ = 'steam_profile'
-    id = Column(Integer, primary_key=True)
-    when = Column(DateTime, nullable=False)
-    #level = Column(Integer)
-    game_count = Column(Integer)
-    achievement_count = Column(Integer)
-    badge_count = Column(Integer)
-    completion_rate = Column(Integer)
-    game_card_count = Column(Integer)
-    guide_count = Column(Integer)
-    perfect_game_count = Column(Integer)
-    review_count = Column(Integer)
-    screenshot_count = Column(Integer)
+	__tablename__ = 'steam_profile'
+	id = Column(Integer, primary_key=True)
+	when = Column(DateTime, nullable=False)
+	#level = Column(Integer)
+	game_count = Column(Integer)
+	achievement_count = Column(Integer)
+	badge_count = Column(Integer)
+	completion_rate = Column(Integer)
+	game_card_count = Column(Integer)
+	guide_count = Column(Integer)
+	perfect_game_count = Column(Integer)
+	review_count = Column(Integer)
+	screenshot_count = Column(Integer)
 
 # check STATS_DIR
 stats_dir = os.environ['STATS_DIR']
@@ -64,7 +64,7 @@ if not os.path.isdir(stats_dir):
 	raise NoStatsDir
 
 print stats_dir, "exists"
- 
+
 # Create an engine that stores data in the $STATS_DIR
 sqlite_file = stats_dir
 sqlite_file += '/steam_games.db'
@@ -97,7 +97,7 @@ session.commit()
 # read games JSON into DB
 steam_games = stats_dir + '/steam_games.json'
 with open(steam_games, 'r') as content_file:
-    games_content = content_file.read()
+	games_content = content_file.read()
 games = json.loads(games_content)
 
 for g in games:
@@ -118,7 +118,7 @@ session.commit()
 # read game stats JSON into DB
 steam_stats = stats_dir + '/steam.json'
 with open(steam_stats, 'r') as content_file:
-    stats_content = content_file.read()
+	stats_content = content_file.read()
 stats = json.loads(stats_content)
 
 for t in stats:
@@ -131,9 +131,9 @@ for t in stats:
 		'when': when_obj,
 		'game_count': stats[t]['Games'],
 		'achievement_count': stats[t]['Achievements'],
-		'badge_count': stats[t].get('Badges Earned',0),
-		'game_card_count': stats[t].get('Game Cards',0),
-		'guide_count': stats[t].get('Guides',0),
+		'badge_count': stats[t].get('Badges Earned', 0),
+		'game_card_count': stats[t].get('Game Cards', 0),
+		'guide_count': stats[t].get('Guides', 0),
 		'perfect_game_count': stats[t]['Perfect Games'],
 		'review_count': stats[t]['Reviews'],
 		'screenshot_count': stats[t]['Screenshots'],
@@ -162,7 +162,7 @@ for stats in session.query(SteamProfile).order_by(SteamProfile.when):
 
 # matplotlib defaults
 matplotlib.rcParams.update({'font.size': 10})
-plt.figure(num=None, figsize=(8.5,3), dpi=100)
+plt.figure(num=None, figsize=(8.5, 3), dpi=100)
 
 # make a new chart
 plt.xlabel('days')
