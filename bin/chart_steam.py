af2b059b6171e02a5c48d0482af07e45109bdaea
Christopher Hicks <chicks@uber.com>
2015-09-02 17:30:40 -0700

    python: resize PNG :ribbon:

--- bin/chart_steam.py
+++ bin/chart_steam.py
@@ -154,6 +154,7 @@ for stats in session.query(SteamProfile).order_by(SteamProfile.when):
 	whens.append(stats.when)
 	counts.append(stats.game_count)
 
+plt.figure(num=None, figsize=(85,30), dpi=10)
 plt.plot(whens,counts)
 png_filename = 'Dash/steam_games.png'
 plt.savefig(png_filename)
